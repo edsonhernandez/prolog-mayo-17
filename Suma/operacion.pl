e0a9983ae6adeded169689ec4fe163a5e213ebbd
EDSON ARATH HERNANDEZ AGUIRRE <EDSON ARATH HERNANDEZ AGUIRRE>
2017-05-04 08:37:53 -0500

    Primer programa

---
+++ Suma/operacion.pl
@@ -0,0 +1,4 @@
+suma(Sumando1,Sumando2,Resultado):-Resultado is Sumando1 + Sumando2.
+resta(Restando1,Restando2,Resultado):-Resultado is Restando1 - Restando2.
+multiplicacion(Multiplica1,Multiplica2,Resultado):-Resultado is Multiplica1 * Multiplica2.
+division(Divide1,Divide2,Resultado):-Resultado is Divide1 / Divide2.
